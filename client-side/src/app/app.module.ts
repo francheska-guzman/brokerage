4a5ab6f35865f1d57b96e945f60efd4f59be38a3
Francheska <gffrancheska@outlook.com>
2017-07-02 16:36:50 -0400

    Updating

--- client-side/src/app/app.module.ts
+++ client-side/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { RouterModule, Routes } from '@angular/router';
+
 @NgModule({
   declarations: [
     AppComponent
