7c2d2070980f03110e041728b543d339242fbb8d
Francheska <gffrancheska@outlook.com>
2017-07-01 20:53:03 -0400

    Creating components.

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { WatchlistComponent } from './watchlist/watchlist.component';
+import { NewsComponent } from './news/news.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    FooterComponent,
+    PortfolioComponent,
+    WatchlistComponent,
+    NewsComponent
   ],
   imports: [
     BrowserModule
