dedf6c0723ccd5ccb34f1479a76f4257e7af262b
Francheska <francheska-guzman@users.noreply.github.com>
2017-11-04 12:00:09 -0400

    Updating

--- client-side/src/app/app.module.ts
+++ client-side/src/app/app.module.ts
@@ -9,7 +9,7 @@ import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './pagenotfound/pagenotfound.component';
 
 const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '/home', component: HomeComponent },
   { path: '**', component: PageNotFoundComponent }
 ];
 
