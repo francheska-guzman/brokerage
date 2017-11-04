2c2f542f5b3fe7b1ef878a5a4750cbaeb9d02aeb
Francheska <francheska-guzman@users.noreply.github.com>
2017-11-04 11:54:48 -0400

    Updating

--- client-side/src/app/app.module.ts
+++ client-side/src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { LogoComponent } from './logo/logo.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './pagenotfound/pagenotfound.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    LogoComponent
+    LogoComponent,
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule
---
+++ client-side/src/app/pagenotfound/pagenotfound.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pagenotfound',
+  templateUrl: './pagenotfound.component.html',
+  styleUrls: ['../app.component.css', './pagenotfound.component.css']
+})
+export class PageNotFoundComponent {
+}
