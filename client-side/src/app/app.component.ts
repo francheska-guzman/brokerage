eb05bbc2173f13d37679196dee955ed92316c567
Francheska <francheska-guzman@users.noreply.github.com>
2017-11-04 12:37:04 -0400

    Updating

--- client-side/src/app/app.component.ts
+++ client-side/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { LogoComponent } from './logo/logo.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { HomeComponent } from './home/home.component';
@@ -7,7 +7,7 @@ import { PageNotFoundComponent } from './pagenotfound/pagenotfound.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
  
